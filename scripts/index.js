e24074fc0fd53b93a990fffaf54dcca42420a05e
Anna Filippova <afilippova240@gmail.com>
2024-10-20 08:29:34 +0300

    элемент шаблона вынесен вне фукции

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,6 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+const cardTemplate = document.querySelector('#card-template').content;
 
 function createCard(cardData, deleteCard) {
-    const cardTemplate = document.querySelector('#card-template').content;
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
     const cardImage = cardElement.querySelector('.card__image');
